bcd472f9b611b3457243df95b8d5126f72197150
Emily Yao <emily2004.yao@gmail.com>
2021-03-17 00:36:20 -0500

    making it live

---
+++ css-and-js/functions.js
@@ -0,0 +1,15 @@
+function goHome() {
+    window.location='https://emilyyao.me/home.html';
+}
+
+function goToResume() {
+    window.location='https://emilyyao.me/resume.html';
+}
+
+function goToProjects() {
+    window.location='https://emilyyao.me/projects.html';
+}
+
+function goToBlog() {
+    window.location='https://emilyyao.me/blog.html';
+}
---
+++ notes.md
@@ -0,0 +1,4 @@
+### todo
+- add in footer
+
+### fixme
